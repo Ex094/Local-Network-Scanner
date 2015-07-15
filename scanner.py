03ccb79f21baf2847b1e841a510f9b2cc30cc181
Muhammad Aslam Khan <Ex094@users.noreply.github.com>
2015-07-15 10:52:27 +0500

    Updated Code

--- scanner.py
+++ scanner.py
@@ -3,33 +3,38 @@
 
 import socket, os, re
 from ping import *
-from time import sleep
 
-#Fetch the Local IP Address
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.connect(('google.com', 0))
+
 ip = s.getsockname()[0]
 
-end = re.search('^[\d]{1,3}.[\d]{1,3}.[\d]{1,3}', ip)
+#Get the Local IP
+end = re.search('^[\d]{1,3}.[\d]{1,3}.[\d]{1,3}.[\d]{1,3}', ip)
 
+#Chop down the last IP Digits
+create_ip = re.search('^[\d]{1,3}.[\d]{1,3}.[\d]{1,3}.', ip)
+
+#Print IP to the user
 print "Your IP Address is: " + str(end.group(0))
 
-#Function toping the IP Address
+#Pinging the IP
 def ping(ip):
-    if  verbose_ping(ip) == True:
-        print ip, 'is Online'
+	if verbose_ping(ip) == True:
+		print ip, 'is Online'
 
-def check(ip):
-
-    if (ip[:7] == '192.168') or (ip[:7] == '172.16') or (ip[:7] == '172.31') or (ip[:2] == '10'):
-        ping(ip)
-    else:
-        print "Either your IP is a Loop Back or it does not belong in local IP range"
-
+#Check IP 
+def CheckLoopBack(ip):
+	if (end.group(0) == '127.0.0.1'):
+		return True
+		print "Either your IP is a Loop Back or it does not belong in local IP range"
 
 print "Pinging IP's..."
 
-while True:
-    for i in range(0, 4):
-        ping(str(end.group(0)) + '.' + str(i))
-    os.system('cls')
+
+if(CheckLoopBack(create_ip)):
+	print "Either your IP is a Loop Back or it does not belong in local IP range"
+else:
+	for i in range(1, 244):
+		ping(str(create_ip.group(0)) + str(i))
+	os.system('clear')
